3d462d721d2fa0a82dc2206b7bb5fc022ccc4449
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-03-29 14:30:06 +0300

    Added changes to Startup class

--- FileOperation/FileOperation/Startup.cs
+++ FileOperation/FileOperation/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,16 +29,17 @@ namespace FileOperation
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
-            
-            services.AddSingleton(x => new BlobServiceClient(Configuration.GetValue<string>("")));
+            //services.AddControllers();
+            services.AddControllersWithViews();
 
-            services.AddCors();
-            /*
-            services.AddSwaggerGen(c =>
+            //services.AddCors();
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FileOperation", Version = "v1" });
-            });*/
+                configuration.RootPath = "ClientApp/dist";
+            });
+
+            services.AddSingleton(x => new BlobServiceClient(Configuration.GetValue<string>("")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,22 +48,48 @@ namespace FileOperation
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                
+
                 //app.UseSwagger();
                 //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FileOperation v1"));
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
+
 
             app.UseRouting();
 
-            app.UseAuthorization();
-            app.UseDefaultFiles(); // <-- Это
-            app.UseStaticFiles(); // <-- Вот это
-            app.UseCors(builder => builder.WithOrigins("http://localhost:4200/"));
+            //app.UseAuthorization();
+            //app.UseDefaultFiles(); // <-- Это
+            // // <-- Вот это
+            //app.UseCors(builder => builder.WithOrigins("http://localhost:4200/"));
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
             });
         }
     }
