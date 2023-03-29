8aff967434ed868fccb0f8bc2f2fdff65a4e7823
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-03-29 14:35:26 +0300

    Added DataUser model

---
+++ FileOperation/FileOperation/Models/DataUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace FileOperation.Models
+{
+    public class DataUser
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public IFormFile Picture { get; set; }
+    }
+}
